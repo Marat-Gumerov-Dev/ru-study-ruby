110dd7d6baa49f3dd9041cd6c2a0d0c3725a7e43
Marat Gumerov <gumerovm74dev@gmail.com>
2022-09-20 11:46:03 +0300

    arrays task added

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,12 +2,40 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = max_of(array)
+        array.collect { |el| el != max && el.positive? ? max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        low = 0
+        high = array.size - 1
+        binary_search(array, low, high, query)
       end
-    end
+
+          private
+
+      def max_of(array)
+        max = array[0]
+        array.each do |el|
+          max = el if el > max
+        end
+        max
+      end
+
+      def binary_search(array, low, high, query)
+        if low <= high
+          middle = (low + high) / 2
+          if array[middle] == query
+            middle
+          elsif query < array[middle]
+            binary_search(array, low, middle - 1, query)
+          else
+            binary_search(array, middle + 1, high, query)
+          end
+        else
+          -1
+        end
+      end
+        end
   end
 end

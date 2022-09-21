bcece8c5510400a733ba8a51a3c049a8a86598f7
Marat Gumerov <gumerovm74dev@gmail.com>
2022-09-21 12:09:26 +0300

    middle rating done

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,14 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        array = CSV.readlines('./test/fixtures/films.csv', headers: true)
+
+        count = array.map do |film|
+          if !film['country'].nil? && film['country'].split(',').length > 1 && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive?
+            film['rating_kinopoisk'].to_f
+          end
+        end.compact
+        count.inject(:+) / count.length
       end
 
       def chars_count(_films, _threshold)

2927dbbbc8c5056c3092ad4a3218a37da71d1274
Marat Gumerov <gumerovm74dev@gmail.com>
2022-09-23 10:50:38 +0300

    added task fp 1

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,16 +5,19 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        count = array.map do |film|
-          if !film['country'].nil? && film['country'].split(',').length > 1 && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive?
-            film['rating_kinopoisk'].to_f
-          end
-        end.compact
-        count.inject(:+) / count.length
+        filtered = array.reject { |film| film['country'].nil? }
+                        .select { |film| film['country'].split(',').length > 1 }
+                        .reject { |film| film['rating_kinopoisk'].nil? }
+                        .select { |film| film['rating_kinopoisk'].to_f.positive? }
+        result = filtered.map { |film| film['rating_kinopoisk'].to_f }
+        result.reduce(:+) / result.length
       end
 
       def chars_count(films, threshold)
+        filtered = films.reject { |film| film['rating_kinopoisk'].nil? }
+                        .select { |film| film['rating_kinopoisk'].to_f >= threshold }
         count = films.map do |film|
+          
           next unless !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
 
           film['name'].chars.select do |char|

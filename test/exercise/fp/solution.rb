bd69e1bc385ec9522d9862fa5b68387046623180
Marat Gumerov <gumerovm74dev@gmail.com>
2022-09-27 22:42:30 +0300

    refactored methods fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,23 +5,23 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        def terms?(film)
-          true if !film['country'].nil? && !film['rating_kinopoisk'].nil? &&
-                  film['country'].split(',').length > 1 && film['rating_kinopoisk'].to_f.positive?
-        end
-
         filtered = array.collect do |film|
-          terms?(film) ? film['rating_kinopoisk'].to_f : nil
+          conditions_film?(film) ? film['rating_kinopoisk'].to_f : nil
         end.compact
         filtered.reduce(:+) / filtered.length
       end
 
       def chars_count(films, threshold)
-        count = films.map do |film|
-          (!film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold) ?
-          film['name'].chars.select { |char| char == 'и' }.size :
-          nil
+        films.map do |film|
+          film['name'].chars.select { |char| char == 'и' }.size if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
         end.compact.inject(:+)
+      end
+
+      private
+
+      def conditions_film?(film)
+        !film['country'].nil? && !film['rating_kinopoisk'].nil? &&
+          film['country'].split(',').length > 1 && film['rating_kinopoisk'].to_f.positive?
       end
     end
   end
